6df80bc861bfc1d926ff7836ed734b21b94a2997
Arya Chowdhury <acschool.445@gmail.com>
2023-08-18 18:25:57 -0400

    Initial Commit

---
+++ Lists/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Lists
+//
+//  Created by scholar on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private var colorsArray: [Color] = [.pink, .blue, .cyan, .mint, .indigo]
+    
+    var body: some View {
+        
+        NavigationStack  {
+           
+            
+            
+            
+            List(colorsArray, id: \.self) {
+                individualColor in
+                
+               
+                NavigationLink(destination: individualColor.ignoresSafeArea()) {
+                    
+                    Text(individualColor.description)
+                }
+                    
+                    
+            }.navigationTitle("Cool Colors").listStyle(.plain)
+
+                
+            }
+            
+        }
+        
+        
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by scholar on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
